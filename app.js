f2e089e20b176d909cee59b333f505b8f36b9e6e
Lucia Morena <luciamorena77@gmail.com>
2022-08-29 16:56:09 -0300

    commit 3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use('/site', require('express').static('site'));
 
 app.get('/',(req,res)=>{
     res.status(200).json({
-        msg:'API - 1.0.0',
+        msg:'API - 2.0.0',
         os:os.hostname(),
         valor:Math.random()
     })
